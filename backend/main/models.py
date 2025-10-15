359f60830798753635cbe05dd2ffb2a9325bab4f
Joynik <insiteam.com.ua@gmail.com>
2025-10-15 22:15:25 +0300

    feat(models): add Color and ProductColor models to manage product colors

--- backend/main/models.py
+++ backend/main/models.py
@@ -20,7 +20,6 @@ class Product(models.Model):
     slug = models.SlugField(unique=True)
     description = models.TextField()
     category = models.ForeignKey(Category, related_name="products", on_delete=models.CASCADE)
-    color = models.CharField(max_length=50)
     main_image = models.ImageField(upload_to="products/main/")
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -42,6 +41,13 @@ class Size(models.Model):
         return self.name
 
 
+class Color(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
 class ProductSize(models.Model):
     product = models.ForeignKey(Product, related_name="product_size", on_delete=models.CASCADE)
     size = models.ForeignKey(Size, on_delete=models.CASCADE)
@@ -52,6 +58,14 @@ class ProductSize(models.Model):
 
     def __str__(self):
         return f"{self.size.name} ({self.stock} in stock) for {self.product.name}"
+
+
+class ProductColor(models.Model):
+    product = models.ForeignKey(Product, related_name="product_color", on_delete=models.CASCADE)
+    color = models.ForeignKey(Color, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Color for {self.product.name}"
 
 
 class ProductImage(models.Model):
